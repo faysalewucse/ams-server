cb9ef0d6f4936dd9ff366123ef53c4bc4d4b1092
faysalewucse <faysal.ewucse@gmail.com>
2023-10-31 11:31:05 +0600

    add await

--- index.js
+++ index.js
@@ -1057,7 +1057,7 @@ async function run() {
     });
 
     // Send a ping to confirm a successful connection
-    client.db("admin").command({ ping: 1 });
+    await client.db("admin").command({ ping: 1 });
     console.log("Successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
